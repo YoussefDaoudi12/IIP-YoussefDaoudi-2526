f0051b8bbe16a42bcd32774998221c0d21ac2ae9
YoussefDaoudi12 <youssef.daoudi@student.odisee.be>
2025-10-01 10:31:18 +0200

    commit week1 tijdens les

--- IIP1.01.Basis/ConsoleDrankkaart/program.cs
+++ IIP1.01.Basis/ConsoleDrankkaart/program.cs
@@ -6,14 +6,14 @@ namespace ConsoleNaamVanDeOefening
    {
       static void Main(string[] args)
       {
-         string header = @"
-		@====== MENU ======
-		☕Koffie:	3€
-		🍵Thee:		2€
-		🥤Limo:		2€
-		------------------";
+         Console.WriteLine(
+@"====== MENU ======
+☕Koffie:   	3€
+🍵Thee:     	2€
+🥤Limo:     	2€
+------------------");
+
 		
-		Console.WriteLine(header);
 		Console.WriteLine("copyright ©2025");
 		Console.WriteLine("Het menu staat ook op \"C:\\Dropbox\\MijnCafe\\menu.pdf\"");
 		
