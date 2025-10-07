0cab15fd12a3a517a97d6e09c199ec6c4de32136
YoussefDaoudi12 <youssef.daoudi@student.odisee.be>
2025-10-07 23:59:28 +0200

    Update Program.cs

--- IIP1.02.Variabelen/ConsoleContactCard/Program.cs
+++ IIP1.02.Variabelen/ConsoleContactCard/Program.cs
@@ -1,23 +1,41 @@
 using System;
 
-namespace ConsoleNaamVanDeOefening
+namespace ConsoleContactCard
 {
    class Program
    {
       static void Main(string[] args)
       {
-         Console.WriteLine(
-@"@====== MENU ======
-☕Koffie:   	3€
-🍵Thee:     	2€
-🥤Limo:     	2€
-------------------");
+          string naam = "Bobby Peru";
+          bool gehuwd = true;
+          string telefoon = "0486/33.22.19";
+          int leeftijd = 25;
+          decimal salaris = 2500.00m; // decimal voor geld
+          char geslacht = 'm';
+          double lengte = 1.75;
 
-		
-		Console.WriteLine("copyright ©2025");
-		Console.WriteLine(" ");
-		Console.WriteLine("Het menu staat ook op \"C:\\Dropbox\\MijnCafe\\menu.pdf\"");
-		
+          string gehuwdTekst = gehuwd ? "Ja" : "Nee";
+
+          Console.ForegroundColor = ConsoleColor.Yellow;
+
+          Console.WriteLine($@"
+----------------
+*
+* Naam: {naam}
+* Gehuwd: {gehuwdTekst}
+* Telefoon: {telefoon}
+* Leeftijd: {leeftijd} jaar
+* Salaris: {salaris:C} per maand
+* Geslacht: {geslacht}
+* Lengte: {lengte:F2}m
+*
+----------------
+");
+
+          Console.ResetColor();
+          Console.WriteLine("Druk op een toets om verder te gaan...");
+          Console.ReadKey();
       }
    }
 }
+
